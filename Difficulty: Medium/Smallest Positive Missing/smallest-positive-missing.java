6483f19d27a16a21417ca3f5f4641d241b34cca4
Madhumitha Reddy <155007336+madhumithareddy23@users.noreply.github.com>
2025-07-22 19:17:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int missingNumber(int[] arr) {
+    int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            while (arr[i] > 0 && arr[i] <= n && arr[arr[i] - 1] != arr[i]) {
+                // Swap arr[i] with arr[arr[i] - 1]
+                int temp = arr[arr[i] - 1];
+                arr[arr[i] - 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        // Find the first missing positive
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // If all positions are correct
+        return n + 1;
+    }
+}
