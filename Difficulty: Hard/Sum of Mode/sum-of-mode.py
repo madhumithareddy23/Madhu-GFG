208b342bff83d3d74b562455bda6503957f8298a
Madhumitha Reddy <155007336+madhumithareddy23@users.noreply.github.com>
2025-09-02 18:36:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Sum of Mode/sum-of-mode.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sumOfModes(self, arr, k):
+        # code here
+        from collections import Counter
+        from heapq import heapify, heappush, heappop
+        if k == 1:
+            return sum(arr)
+        n = len(arr)
+        freqs = Counter(arr[:k])
+        h = [(-count, a) for a, count in freqs.items()]
+        heapify(h)
+        modes_sum = h[0][1]
+        for i in range(k, n):
+            first, last = arr[i - k], arr[i]
+            freqs[first] -= 1
+            freqs[last] += 1
+            heappush(h, (-freqs[first], first))
+            heappush(h, (-freqs[last], last))
+            while -h[0][0] != freqs[h[0][1]]:
+                heappop(h)
+            modes_sum += h[0][1]
+        return modes_sum
