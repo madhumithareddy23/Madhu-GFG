5181728014b9cac864b618efdfb684fca0afc9e0
Madhumitha Reddy <155007336+madhumithareddy23@users.noreply.github.com>
2025-09-02 18:30:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Swap Kth nodes from ends/swap-kth-nodes-from-ends.py
@@ -0,0 +1,33 @@
+'''
+class Node:
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+'''
+class Solution:
+    def swapKth(self, head, k):
+        # code here
+        ptr=head
+        val=0
+        while ptr:
+            val+=1
+            if val==k:
+                break
+            ptr=ptr.next
+        
+        if ptr is None:
+            return head
+            
+        slow=head
+        fast=head
+        while k>0 and fast:
+            fast=fast.next
+            k-=1
+            
+        while fast:
+            slow=slow.next
+            fast=fast.next
+        slow.data,ptr.data=ptr.data,slow.data
+        
+        return head
+        
