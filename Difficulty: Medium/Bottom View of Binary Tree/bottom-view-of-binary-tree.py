bbb48c75103964a6418396750f3730438ae419ae
Madhumitha Reddy <155007336+madhumithareddy23@users.noreply.github.com>
2025-10-07 22:55:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.py
@@ -0,0 +1,21 @@
+class Solution:
+    def __init__(self):
+        self.map = {}
+
+    def traverse(self, root, val = 0, depth = 0):
+        if val not in self.map or self.map[val][0] <= depth:
+            self.map[val] = (depth, root.data)
+        
+        if root.left:
+            self.traverse(root.left, val - 1, depth + 1)
+            
+        if root.right:
+            self.traverse(root.right, val + 1, depth + 1)
+        
+    def bottomView(self, root):
+        # code here
+        res = []
+        self.traverse(root)
+        for k in sorted(self.map):
+            res.append(self.map[k][1])
+        return res
